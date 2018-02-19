24d3d3c493cc2d3fe11e124d77612b0a47fe6915
Samy Amraoui <samy.amraoui@ensimag.grenoble-inp.fr>
2018-02-19 15:23:28 +0100

    Display prediction stats

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "predictor.hpp"
 
 #include <iostream>
+#include <ctime>
 
 int main(int argc, char *argv[]) {
     // Read arguments
@@ -32,17 +33,50 @@ int main(int argc, char *argv[]) {
 
     // Prediction matrix
     std::cout << "Matrix factorization" << std::endl;
+
+    std::clock_t start = clock();
     predictor->predictionMatrix(K, alpha, beta, steps);
+    double duration = (double)(clock() - start) / CLOCKS_PER_SEC;
 
     // Test predictions
     std::cout << "===== Test predictions =====" << std::endl;
+    size_t totalResults = 0;
+    size_t goodResults = 0;
+    double disparity = 0;
     for (Rating *rating: parser->getTestRatings()) {
+        // Compute prediction
+        double expected = rating->getMark();
+        double predicted = predictor->predict(
+                    rating->getUser() - 1, rating->getMovie() - 1);
+
+        // Increase counters
+        ++totalResults;
+        if (std::abs(predicted - expected) < 0.25) {
+            ++goodResults;
+        }
+
+        // Prediction disparity
+        disparity += std::abs(predicted - expected);
+
+        // Print rating result
         std::cout << "Rating: " << rating->toString()
-                  << "\t expected: " << rating->getMark()
-                  << "\t predicted: " << predictor->predict(
-                             rating->getUser() - 1, rating->getMovie() - 1)
+                  << "\t expected: " << expected
+                  << "\t predicted: " << predicted
                   << std::endl;
     }
+
+    // Average prediction disparity
+    disparity /= totalResults;
+
+    // Print good results ratio
+    std::cout << "Total results: " << totalResults << std::endl
+              << "Good results (+- 0.25): " << goodResults
+              << " (" << (double)(goodResults) / totalResults * 100 << " %)" << std::endl
+              << "Average disparity: " << disparity
+              << std::endl;
+
+    std::cout << "============================" << std::endl;
+    std::cout << "Factorization duration: " << duration << " s" << std::endl;
 
     // Free memory
     delete parser;
